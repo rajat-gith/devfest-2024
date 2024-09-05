82e9dfcef89b2fd6cbb1301a2da5b49969a440ea
rajat-gith <rajatnayak1582002@gmail.com>
2024-09-06 00:29:35 +0530

    added firebase config

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBSa-oMlat_PFPgtReLp6enVs8kt5Q8x3U",
+  authDomain: "ccd-app-2024.firebaseapp.com",
+  projectId: "ccd-app-2024",
+  storageBucket: "ccd-app-2024.appspot.com",
+  messagingSenderId: "1028017059751",
+  appId: "1:1028017059751:web:5339d59cdd5194ad2ee882",
+  measurementId: "G-HVFMR4KQC6",
+};
