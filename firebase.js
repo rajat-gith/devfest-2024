91426a1e0b177e6c66aa025214352c880dbd7098
rajat-gith <rajatnayak1582002@gmail.com>
2024-09-06 18:44:41 +0530

    modified firebase config

--- firebase.js
+++ firebase.js
@@ -1,16 +1,18 @@
-import { initializeApp } from "firebase/app";
+import { getApps, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./firebase.config";
+import { firebaseConfig } from "./firebase.config"; 
 
-// Initialize Firebase app
-const app = initializeApp(firebaseConfig);
+const alreadyCreatedApps = getApps();
 
-// Initialize Firebase services
+const app =
+  alreadyCreatedApps.length === 0
+    ? initializeApp(firebaseConfig)
+    : alreadyCreatedApps[0];
+
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app); // Initialize Firebase Storage
+const storage = getStorage(app);
 
-// Export Firebase services
 export { auth, db, storage };
