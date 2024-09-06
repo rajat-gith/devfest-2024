42c41659b84810deb76b6b799f4deba1fb7f2e25
rajat-gith <rajatnayak1582002@gmail.com>
2024-09-06 10:09:04 +0530

    modified firebase config

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+export const firebaseApp = initializeApp({
   apiKey: "AIzaSyBSa-oMlat_PFPgtReLp6enVs8kt5Q8x3U",
   authDomain: "ccd-app-2024.firebaseapp.com",
   projectId: "ccd-app-2024",
@@ -11,19 +11,10 @@ const firebaseConfig = {
   messagingSenderId: "1028017059751",
   appId: "1:1028017059751:web:5339d59cdd5194ad2ee882",
   measurementId: "G-HVFMR4KQC6",
-};
+});
 
-// Check if a Firebase app is already initialized
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0]; // Use existing app if already initialized
-}
-
-// Initialize Firebase services
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
 
 export { auth, db, storage };
