5630a674943f2c7d95f1820bdb762464c9a17b74
rajat-gith <rajatnayak1582002@gmail.com>
2024-09-06 09:37:23 +0530

    modified firebase config

--- firebase.js
+++ firebase.js
@@ -1,16 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // Import Firebase Storage
-import { firebaseConfig } from "./firebase.config";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
-// Initialize Firebase app
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase services
+// Example usage of Firebase services
 const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app); // Initialize Firebase Storage
-
-// Export Firebase services
-export { auth, db, storage };
+const firestore = getFirestore(app);
